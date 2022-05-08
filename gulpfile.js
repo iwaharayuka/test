95b5edb9610b62d09c43cd2ad5051d19b1c088eb
iwaharayuka <56747849+iwaharayuka@users.noreply.github.com>
2022-05-08 21:41:34 +0900

    v1

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,18 @@
-// gulpプラグインを読み込みます
-const { src, dest, watch } = require("gulp");
-// Sassをコンパイルするプラグインを読み込みます
-const sass = require("gulp-sass")(require("sass"));
+// gulpプラグインの呼び出し
+const { src, dest, watch, series, parallel } = require("gulp");
+// Sassをコンパイルするプラグインの呼び出し
+const sass = require('gulp-sass')(require('sass'));
+// ベンダープレフィックスを付与するプラグインの呼び出し
+const autoprefixer = require('gulp-autoprefixer');
+// 画像を圧縮するプラグインの呼び出し
+const imagemin = require('gulp-imagemin');
+const pngquant = require('imagemin-pngquant');
+// ファイルの変更を監視して、変更をブラウザーに反映させるプラグインの呼び出し
+const browserSync = require('browser-sync');
+
 
 /**
- * Sassをコンパイルするタスクです
+ * Sassをコンパイルするタスク
  */
 const compileSass = () =>
   // style.scssファイルを取得
@@ -16,13 +24,61 @@ const compileSass = () =>
         outputStyle: "expanded"
       })
     )
-    // distフォルダー以下のcssフォルダーに保存
+    // ベンダープレフィックス付与を実行
+    .pipe(
+      autoprefixer([
+        // 各ブラウザの2世代前までのバージョンを担保
+        'last 2 versions',
+        // IE11を担保
+        'ie >= 11'
+      ])
+    )
+    // dist以下のcssフォルダーに保存
     .pipe(dest("dist/css"));
 
-/**
- * Sassファイルを監視し、変更があったらSassを変換します
- */
-const watchSassFiles = () => watch("src/scss/style.scss", compileSass);
 
-// npx gulpというコマンドを実行した時、watchSassFilesが実行されるようにします
-exports.default = watchSassFiles;
+/**
+ * 画像を圧縮するタスク
+ */
+const imgMin = () =>
+  // 画像を取得
+  src("src/img/**.jpg")
+  // 画像の圧縮を実行
+  .pipe(
+    imagemin([
+      // JPG画像を80%圧縮
+      imagemin.mozjpeg({ quality: 80 })
+    ])
+  )
+  // dist以下のimgフォルダーに保存
+  .pipe(dest("dist/img"));
+
+
+/**
+ * ファイルの変更を監視して、ブラウザーに反映
+ */
+const browserSyncFunc = () => {
+  browserSync.init(browserSyncOption);
+}
+const browserSyncOption = {
+  server: {
+    baseDir: "./"
+  },
+  startPath: "dist/index.html",
+  open: "external", 
+  reloadOnRestart: true,
+}
+
+// 変更があったら、ブラウザをリロード
+const browserSyncReload = (done) => {
+  browserSync.reload();
+  done();
+ }
+// ファイルの変更を監視
+const watchFiles = () => {
+  watch("src/scss/style.scss", series(compileSass, browserSyncReload))
+  watch("src/img/**", series(imgMin, browserSyncReload))
+}
+
+// npx gulpを実行したら、watchFilesを実行
+exports.default = series(series(compileSass, imgMin), parallel(watchFiles, browserSyncFunc));
